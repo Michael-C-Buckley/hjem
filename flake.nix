e97bf364af3a0671b85f2eea3e14d0b1ca3e0e63
Michael Buckley <michaelcbuckley@proton.me>
2025-01-12 06:33:31 -0500

    Reorganized to prevent infinite recursion

--- flake.nix
+++ flake.nix
@@ -4,7 +4,10 @@
   inputs.hjem.url = "github:feel-co/hjem";
 
   outputs = { hjem, ...}: {
-    nixosModules.default = import ./hjem/desktop.nix { inherit hjem; };
     nixosModules.minimal = import ./hjem/minimal.nix { inherit hjem; };
+    nixosModules.default = import [
+      ./hjem/minimal.nix { inherit hjem; }
+      ./hjem/desktop.nix
+    ];
   };
 }
--- hjem/desktop.nix
+++ hjem/desktop.nix
@@ -1,9 +1,6 @@
 # Adds configs for GUI and other non-Server functionality
-{ hjem, ... }:
+{ ... }:
 {
-  imports = [
-    ./minimal.nix {inherit hjem; }
-  ];
   hjem.users.michael.files = {
     "ghostty/config".source = ./userfiles/ghostty;
   };
