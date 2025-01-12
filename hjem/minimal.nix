d29438e288ce91876f1f239abe9bd6a229ef5fbc
Michael Buckley <michaelcbuckley@proton.me>
2025-01-12 07:29:03 -0500

    Added hjem init section for my user

--- hjem/minimal.nix
+++ hjem/minimal.nix
@@ -4,6 +4,16 @@
     hjem.nixosModules.default
     ./zsh
   ];
+
+  hjem = {
+    clobberByDefault = true;
+    users.michael = {
+      enable = true;
+      user = "michael";
+      directory = "/home/michael";
+    };
+  };
+
   hjem.users.michael.files = {
     ".gitconfig".source = ./userfiles/.gitconfig;
   };
